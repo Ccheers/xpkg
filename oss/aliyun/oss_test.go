c7b914c18ca1b22f0cef4c13c4d76c8c8fcaa3a1
eric <1048315650@qq.com>
2023-04-01 16:12:54 +0800

    fix: test

--- oss/aliyun/oss_test.go
+++ oss/aliyun/oss_test.go
@@ -1,127 +1,130 @@
 package aliyun
 
-import (
-	"sync"
-	"testing"
-
-	"github.com/aliyun/aliyun-oss-go-sdk/oss"
-)
-
-var testClient *oss.Client
-
-var once = sync.Once{}
-
-const (
-	testBucket = ""
-	testKey    = ""
-)
-
-func InitTestProvider() {
-	once.Do(func() {
-		ak := ""
-		sk := ""
-		endpoint := "http://oss-cn-hangzhou.aliyuncs.com"
-		var err error
-		testClient, err = oss.New(endpoint, ak, sk)
-		if err != nil {
-			panic(err)
-		}
-	})
-}
-
-func TestOss_Get(t *testing.T) {
-	return
-
-	//InitTestProvider()
-	//
-	//type fields struct {
-	//	client *oss.Client
-	//}
-	//type args struct {
-	//	ctx    context.Context
-	//	bucket string
-	//	key    string
-	//}
-	//tests := []struct {
-	//	name     string
-	//	fields   fields
-	//	args     args
-	//	wantResp []byte
-	//	wantErr  bool
-	//}{
-	//	{
-	//		name: "1",
-	//		fields: fields{
-	//			client: testClient,
-	//		},
-	//		args: args{
-	//			ctx:    context.Background(),
-	//			bucket: testBucket,
-	//			key:    testKey,
-	//		},
-	//		wantResp: []byte("test"),
-	//		wantErr:  false,
-	//	},
-	//}
-	//for _, tt := range tests {
-	//	t.Run(tt.name, func(t *testing.T) {
-	//		o := &Oss{
-	//			client: tt.fields.client,
-	//		}
-	//		gotResp, err := o.Get(tt.args.ctx, tt.args.bucket, tt.args.key)
-	//		if (err != nil) != tt.wantErr {
-	//			t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
-	//			return
-	//		}
-	//		if !reflect.DeepEqual(gotResp, tt.wantResp) {
-	//			t.Errorf("Get() gotResp = %v, want %v", gotResp, tt.wantResp)
-	//		}
-	//	})
-	//}
-}
-
-func TestOss_Set(t *testing.T) {
-	return
-	//
-	//InitTestProvider()
-	//
-	//type fields struct {
-	//	client *oss.Client
-	//}
-	//type args struct {
-	//	ctx    context.Context
-	//	bucket string
-	//	key    string
-	//	reader io.Reader
-	//}
-	//tests := []struct {
-	//	name    string
-	//	fields  fields
-	//	args    args
-	//	wantErr bool
-	//}{
-	//	{
-	//		name: "1",
-	//		fields: fields{
-	//			client: testClient,
-	//		},
-	//		args: args{
-	//			ctx:    context.Background(),
-	//			bucket: testBucket,
-	//			key:    testKey,
-	//			reader: strings.NewReader("test"),
-	//		},
-	//		wantErr: false,
-	//	},
-	//}
-	//for _, tt := range tests {
-	//	t.Run(tt.name, func(t *testing.T) {
-	//		o := &Oss{
-	//			client: tt.fields.client,
-	//		}
-	//		if err := o.Set(tt.args.ctx, tt.args.bucket, tt.args.key, tt.args.reader); (err != nil) != tt.wantErr {
-	//			t.Errorf("Set() error = %v, wantErr %v", err, tt.wantErr)
-	//		}
-	//	})
-	//}
-}
+//
+//import (
+//	"context"
+//	"io"
+//	"reflect"
+//	"strings"
+//	"sync"
+//	"testing"
+//
+//	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+//)
+//
+//var testClient *oss.Client
+//
+//var once = sync.Once{}
+//
+//const (
+//	testBucket = ""
+//	testKey    = ""
+//)
+//
+//func InitTestProvider() {
+//	once.Do(func() {
+//		ak := ""
+//		sk := ""
+//		endpoint := "http://oss-cn-hangzhou.aliyuncs.com"
+//		var err error
+//		testClient, err = oss.New(endpoint, ak, sk)
+//		if err != nil {
+//			panic(err)
+//		}
+//	})
+//}
+//
+//func TestOss_Get(t *testing.T) {
+//
+//	InitTestProvider()
+//
+//	type fields struct {
+//		client *oss.Client
+//	}
+//	type args struct {
+//		ctx    context.Context
+//		bucket string
+//		key    string
+//	}
+//	tests := []struct {
+//		name     string
+//		fields   fields
+//		args     args
+//		wantResp []byte
+//		wantErr  bool
+//	}{
+//		{
+//			name: "1",
+//			fields: fields{
+//				client: testClient,
+//			},
+//			args: args{
+//				ctx:    context.Background(),
+//				bucket: testBucket,
+//				key:    testKey,
+//			},
+//			wantResp: []byte("test"),
+//			wantErr:  false,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			o := &Oss{
+//				client: tt.fields.client,
+//			}
+//			gotResp, err := o.Get(tt.args.ctx, tt.args.bucket, tt.args.key)
+//			if (err != nil) != tt.wantErr {
+//				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
+//				return
+//			}
+//			if !reflect.DeepEqual(gotResp, tt.wantResp) {
+//				t.Errorf("Get() gotResp = %v, want %v", gotResp, tt.wantResp)
+//			}
+//		})
+//	}
+//}
+//
+//func TestOss_Set(t *testing.T) {
+//
+//	InitTestProvider()
+//
+//	type fields struct {
+//		client *oss.Client
+//	}
+//	type args struct {
+//		ctx    context.Context
+//		bucket string
+//		key    string
+//		reader io.Reader
+//	}
+//	tests := []struct {
+//		name    string
+//		fields  fields
+//		args    args
+//		wantErr bool
+//	}{
+//		{
+//			name: "1",
+//			fields: fields{
+//				client: testClient,
+//			},
+//			args: args{
+//				ctx:    context.Background(),
+//				bucket: testBucket,
+//				key:    testKey,
+//				reader: strings.NewReader("test"),
+//			},
+//			wantErr: false,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			o := &Oss{
+//				client: tt.fields.client,
+//			}
+//			if err := o.Set(tt.args.ctx, tt.args.bucket, tt.args.key, tt.args.reader); (err != nil) != tt.wantErr {
+//				t.Errorf("Set() error = %v, wantErr %v", err, tt.wantErr)
+//			}
+//		})
+//	}
+//}
--- oss/huawei/oss_test.go
+++ oss/huawei/oss_test.go
@@ -1,126 +1,128 @@
 package huawei
 
-import (
-	"sync"
-	"testing"
-
-	"github.com/huaweicloud/huaweicloud-sdk-go-obs/obs"
-)
-
-var testClient *obs.ObsClient
-
-var once = sync.Once{}
-
-const (
-	testBucket = ""
-	testKey    = ""
-)
-
-func InitTestProvider() {
-	once.Do(func() {
-		ak := ""
-		sk := ""
-		endpoint := "https://obs.cn-east-3.myhuaweicloud.com"
-		var err error
-		testClient, err = obs.New(ak, sk, endpoint)
-		if err != nil {
-			panic(err)
-		}
-	})
-}
-
-func TestOss_Get(t *testing.T) {
-	return
-	//InitTestProvider()
-	//
-	//type fields struct {
-	//	client *obs.ObsClient
-	//}
-	//type args struct {
-	//	ctx    context.Context
-	//	bucket string
-	//	key    string
-	//}
-	//tests := []struct {
-	//	name     string
-	//	fields   fields
-	//	args     args
-	//	wantResp []byte
-	//	wantErr  bool
-	//}{
-	//	{
-	//		name: "1",
-	//		fields: fields{
-	//			client: testClient,
-	//		},
-	//		args: args{
-	//			ctx:    context.Background(),
-	//			bucket: testBucket,
-	//			key:    testKey,
-	//		},
-	//		wantResp: []byte("test"),
-	//		wantErr:  false,
-	//	},
-	//}
-	//for _, tt := range tests {
-	//	t.Run(tt.name, func(t *testing.T) {
-	//		h := &Oss{
-	//			client: tt.fields.client,
-	//		}
-	//		gotResp, err := h.Get(tt.args.ctx, tt.args.bucket, tt.args.key)
-	//		if (err != nil) != tt.wantErr {
-	//			t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
-	//			return
-	//		}
-	//		if !reflect.DeepEqual(gotResp, tt.wantResp) {
-	//			t.Errorf("Get() gotResp = %v, want %v", gotResp, tt.wantResp)
-	//		}
-	//	})
-	//}
-}
-
-func TestOss_Set(t *testing.T) {
-	return
-	//
-	//InitTestProvider()
-	//
-	//type fields struct {
-	//	client *obs.ObsClient
-	//}
-	//type args struct {
-	//	ctx    context.Context
-	//	bucket string
-	//	key    string
-	//	reader io.Reader
-	//}
-	//tests := []struct {
-	//	name    string
-	//	fields  fields
-	//	args    args
-	//	wantErr bool
-	//}{
-	//	{
-	//		name: "1",
-	//		fields: fields{
-	//			client: testClient,
-	//		},
-	//		args: args{
-	//			ctx:    context.Background(),
-	//			bucket: testBucket,
-	//			key:    testKey,
-	//			reader: strings.NewReader("test"),
-	//		},
-	//		wantErr: false,
-	//	},
-	//}
-	//for _, tt := range tests {
-	//	t.Run(tt.name, func(t *testing.T) {
-	//		h := &Oss{
-	//			client: tt.fields.client,
-	//		}
-	//		if err := h.Set(tt.args.ctx, tt.args.bucket, tt.args.key, tt.args.reader); (err != nil) != tt.wantErr {
-	//			t.Errorf("Set() error = %v, wantErr %v", err, tt.wantErr)
-	//		}
-	//	})
-	//}
-}
+//
+//import (
+//	"context"
+//	"io"
+//	"reflect"
+//	"strings"
+//	"sync"
+//	"testing"
+//
+//	"github.com/huaweicloud/huaweicloud-sdk-go-obs/obs"
+//)
+//
+//var testClient *obs.ObsClient
+//
+//var once = sync.Once{}
+//
+//const (
+//	testBucket = ""
+//	testKey    = ""
+//)
+//
+//func InitTestProvider() {
+//	once.Do(func() {
+//		ak := ""
+//		sk := ""
+//		endpoint := "https://obs.cn-east-3.myhuaweicloud.com"
+//		var err error
+//		testClient, err = obs.New(ak, sk, endpoint)
+//		if err != nil {
+//			panic(err)
+//		}
+//	})
+//}
+//
+//func TestOss_Get(t *testing.T) {
+//	InitTestProvider()
+//
+//	type fields struct {
+//		client *obs.ObsClient
+//	}
+//	type args struct {
+//		ctx    context.Context
+//		bucket string
+//		key    string
+//	}
+//	tests := []struct {
+//		name     string
+//		fields   fields
+//		args     args
+//		wantResp []byte
+//		wantErr  bool
+//	}{
+//		{
+//			name: "1",
+//			fields: fields{
+//				client: testClient,
+//			},
+//			args: args{
+//				ctx:    context.Background(),
+//				bucket: testBucket,
+//				key:    testKey,
+//			},
+//			wantResp: []byte("test"),
+//			wantErr:  false,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			h := &Oss{
+//				client: tt.fields.client,
+//			}
+//			gotResp, err := h.Get(tt.args.ctx, tt.args.bucket, tt.args.key)
+//			if (err != nil) != tt.wantErr {
+//				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
+//				return
+//			}
+//			if !reflect.DeepEqual(gotResp, tt.wantResp) {
+//				t.Errorf("Get() gotResp = %v, want %v", gotResp, tt.wantResp)
+//			}
+//		})
+//	}
+//}
+//
+//func TestOss_Set(t *testing.T) {
+//	InitTestProvider()
+//
+//	type fields struct {
+//		client *obs.ObsClient
+//	}
+//	type args struct {
+//		ctx    context.Context
+//		bucket string
+//		key    string
+//		reader io.Reader
+//	}
+//	tests := []struct {
+//		name    string
+//		fields  fields
+//		args    args
+//		wantErr bool
+//	}{
+//		{
+//			name: "1",
+//			fields: fields{
+//				client: testClient,
+//			},
+//			args: args{
+//				ctx:    context.Background(),
+//				bucket: testBucket,
+//				key:    testKey,
+//				reader: strings.NewReader("test"),
+//			},
+//			wantErr: false,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			h := &Oss{
+//				client: tt.fields.client,
+//			}
+//			if err := h.Set(tt.args.ctx, tt.args.bucket, tt.args.key, tt.args.reader); (err != nil) != tt.wantErr {
+//				t.Errorf("Set() error = %v, wantErr %v", err, tt.wantErr)
+//			}
+//		})
+//	}
+//}
